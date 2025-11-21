a078930bc74a6e3ef1ad1a15690c60b4d687f688
meavve <sasha.skibel@mail.ru>
2025-11-21 17:54:13 +0300

    aa

---
+++ lesson1/aa.py
@@ -0,0 +1,16 @@
+a = int(input())
+b = int(input())
+c = int(input())
+if  a >= 0:
+    n = a
+else:
+    n <= 0
+if b >= 0:
+    g = b
+else:
+    g <= 0
+if c >= 0:
+    s = c
+else:
+    s <= 0
+    print(n+g+s)
