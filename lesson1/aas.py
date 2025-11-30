3d46295b1887837511d94e26fe5a4d21441987dd
meavve <sasha.skibel@mail.ru>
2025-11-30 16:24:56 +0300

    Update aas.py

--- lesson1/aas.py
+++ lesson1/aas.py
@@ -1,5 +1,8 @@
-n = int(input())
-if (n%4==0 and n!= 100) or n%400==0:
+x1 = int(input())
+y1 = int(input())
+x2 = int(input())
+y2 = int(input())
+if x1 == x2 or y1 == y2:
     print("YES")
 else:
     print("NO")
