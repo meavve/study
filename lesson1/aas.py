081d15036efb4739b0f2446943c55fdd51d1198c
meavve <sasha.skibel@mail.ru>
2025-11-30 19:19:07 +0300

    Update aas.py

--- lesson1/aas.py
+++ lesson1/aas.py
@@ -3,7 +3,7 @@ b = int(input())
 c = int(input())
 if a==b==c:
     print("Равносторонний")
-elif a==b and a!=c:
+elif a==b or b==c or a==c:
     print("Равнобедренный")
 elif a!=b and b!=c and a!=c:
     print("Разносторонний")
