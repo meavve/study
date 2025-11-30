d84c5bd1b1a659612e0d939438f8f3697564e851
meavve <sasha.skibel@mail.ru>
2025-11-30 19:24:21 +0300

    aasxzz

---
+++ lesson1/qadc.py
@@ -0,0 +1,10 @@
+a = int(input())
+b = int(input())
+c = int(input())
+if a > b and a < c or a > c and a < b:
+    print(a)
+elif (b > a and b < c) or (b < a and b > c):
+    print(b)
+elif (c > a and c < b) or (c < a and c > b):
+    print(c)
+
