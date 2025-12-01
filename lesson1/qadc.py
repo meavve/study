ceb4219e522aebc2cba005c938c3c41ba8a142d8
meavve <sasha.skibel@mail.ru>
2025-12-01 17:25:52 +0300

    Update qadc.py

--- lesson1/qadc.py
+++ lesson1/qadc.py
@@ -1,16 +1,9 @@
-a = int(input())
-b = int(input())
-operation = input()
-if operation == "+":
-    print(a+b)
-elif operation == "-":
-    print(a-b)
-elif operation == "*":
-    print(a*b)
-elif b == 0:
-    print("На ноль делить нельзя!")
-elif operation == "/":
-    print(a/b)
-else:
-    print("Неверная операция")
-
+n = int(input())
+if n == 0:
+    print("зеленый")
+elif n == 1 or n == 3 or  n == 5 or  n == 7 or  n == 9 or  n == 12 or  n == 14 or  n == 16 or  n == 18 or  n == 19 or  n == 21 or  n == 23 or  n == 25 or  n == 27 or  n == 28 or  n == 30 or  n == 32 or  n == 34 or  n == 36:
+    print("красный")
+elif   n == 2 or  n == 4 or  n == 6 or  n == 8 or  n == 10 or  n == 13 or  n == 15 or  n == 17 or  n == 20 or  n == 22 or  n == 24 or  n == 26 or  n == 29 or  n == 31 or  n == 33 or  n == 35:
+    print("черный")
+elif n<0 or n>36:
+    print("ошибка ввода")
