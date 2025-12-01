c1d1f407f22c7f71ceffc191352d5b3683a55e1d
meavve <sasha.skibel@mail.ru>
2025-12-01 15:35:23 +0300

    Update qadc.py

--- lesson1/qadc.py
+++ lesson1/qadc.py
@@ -1,7 +1,16 @@
-n = int(input())
-if n>0 and n<60:
-    print("Легкий вес")
-elif n>=60 and n<64:
-    print("Первый полусредний вес")
-elif n>=64 and n<69:
-    print("Полусредний вес")
+a = int(input())
+b = int(input())
+operation = input()
+if operation == "+":
+    print(a+b)
+elif operation == "-":
+    print(a-b)
+elif operation == "*":
+    print(a*b)
+elif b == 0:
+    print("На ноль делить нельзя!")
+elif operation == "/":
+    print(a/b)
+else:
+    print("Неверная операция")
+
