742cebfa9483067c85c86484ce0302cb358a9ddd
meavve <sasha.skibel@mail.ru>
2025-12-06 19:06:48 +0300

    Update vvdvsv.py

--- lesson1/vvdvsv.py
+++ lesson1/vvdvsv.py
@@ -1,9 +1,8 @@
-n = int(input())
-if n%2!=0:
+a1 = int(input())
+a2 = int(input())
+b1 = int(input())
+b2 = int(input())
+if (a1-b1)**2 == (a2 - b2)**2:
     print("YES")
-elif (2 <= n <= 5) and n % 2 == 0:
-    print("NO")
-elif (6 <= n <= 20) and n % 2 == 0:
-    print("YES")
-elif n%2!=0 and n>20:
+else:
     print("NO")
